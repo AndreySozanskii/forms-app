81ee9dd208a683b90e5f5741c5f254fa9945ea24
Andrew Sozanskii <andrewsozanskii@gmail.com>
2025-05-06 12:20:41 +0300

    cleanup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { mockBackendInterceptor } from './interceptors/mock-backend.interceptor';
+import { mockBackendInterceptor } from '@interceptors/mock-backend.interceptor';
 import { provideRouter, Routes } from '@angular/router';
-import { MainPageComponent } from './pages/main-page/main-page.component';
+import { MainPageComponent } from '@pages/main-page';
 
 export const routes: Routes = [
   {
